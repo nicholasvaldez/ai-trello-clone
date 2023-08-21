237be93419f83fe3dd3b41dfe71fb98726b90fc2
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-21 10:42:55 -0500

    add logic to sort columns in helper function

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -1,6 +1,6 @@
 import { databases } from "@/appwrite"
 
-// This function fetches a list of todos from the Appwrite database and groups them by column.
+/* Fetch a list of todos from the Appwrite database and groups them by column */
 export const getTodosGroupedByColumn = async () => {
   const data = await databases.listDocuments(
     // Fetch the list of todos from the Appwrite database.
@@ -9,25 +9,53 @@ export const getTodosGroupedByColumn = async () => {
   )
   const todos = data.documents
 
+  /* Transform into Map object */
+
   const columns = todos.reduce((acc, todo) => {
-    // Check if there is no current acc/map object
     if (!acc.get(todo.status)) {
-      // Set the key-value pair of the map object
       acc.set(todo.status, {
         id: todo.status,
         todos: [],
       })
     }
-    // push the data into the todos array
     acc.get(todo.status)!.todos.push({
       $id: todo.$id,
       $createdAt: todo.$createdAt,
       title: todo.title,
       status: todo.status,
-      // get the image if it exists on the todo
       ...(todo.image && { image: todo.image }),
     })
 
     return acc
   }, new Map<TypedColumn, Column>())
+
+  /* Ensure columns can hold empty arrays */
+
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"]
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        todos: [],
+      })
+    }
+  }
+
+  /* Ensure columns are always sorted in the correct order */
+
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  )
+
+  /* Create new return variable with sorted columns */
+
+  const board: Board = {
+    columns: sortedColumns,
+  }
+
+  /* Return Board */
+
+  return board
 }
